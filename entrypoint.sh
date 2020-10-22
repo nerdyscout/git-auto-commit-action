c305e5373a4db957bfe314addcf961f5feb3037e
Stefan Zweifel <hello@stefanzweifel.io>
2020-10-22 20:24:57 +0200

    Multiline git-commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -63,7 +63,11 @@ _local_commit() {
 
     echo "git -c user.name="$INPUT_COMMIT_USER_NAME" -c user.email="$INPUT_COMMIT_USER_EMAIL" commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR" ${INPUT_COMMIT_OPTIONS:+"${INPUT_COMMIT_OPTIONS_ARRAY[@]}"};";
 
-    git -c user.name="$INPUT_COMMIT_USER_NAME" -c user.email="$INPUT_COMMIT_USER_EMAIL" commit -m "$INPUT_COMMIT_MESSAGE" --author="$INPUT_COMMIT_AUTHOR" ${INPUT_COMMIT_OPTIONS:+"${INPUT_COMMIT_OPTIONS_ARRAY[@]}"};
+    git -c user.name="$INPUT_COMMIT_USER_NAME" -c user.email="$INPUT_COMMIT_USER_EMAIL" \
+        commit -m "$INPUT_COMMIT_MESSAGE" \
+        --author="$INPUT_COMMIT_AUTHOR" \
+        ${INPUT_COMMIT_OPTIONS:+"${INPUT_COMMIT_OPTIONS_ARRAY[@]}"};
+
 }
 
 _tag_commit() {
